9f5fb180007c01aed4e882404728423c736dfd23
technicole-t <thorleynicole@gmail.com>
2020-07-13 07:57:23 +0100

    added journey not found exception

---
+++ src/main/java/com/example/demo/exceptions/JourneyNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.demo.exceptions;
+
+import javax.persistence.EntityNotFoundException;
+
+public class JourneyNotFoundException extends EntityNotFoundException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+}
